07fa30238a815667450c643c45a5289bae1f69ea
Vishnu <vinventor1999@gmail.com>
2023-11-01 21:55:02 +0530

    Basic set up of admin page and user page is done

---
+++ WebShopping/routes/admin.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  let products = [
+    {
+      name: "Samsung S23 Ultra",
+      category: "Mobile",
+      description: "Latest Samsung Phone",
+      image: "https://m.media-amazon.com/images/I/61imYpK33qL.jpg"
+    },
+    {
+      name: "I phone 15 Pro Max",
+      category: "Mobile",
+      description: "Latest Apple Phone",
+      image: "https://m.media-amazon.com/images/I/41lQuD3zXhL._SX300_SY300_QL70_FMwebp_.jpg"
+    },
+    {
+      name: "OnePlus 11R",
+      category: "Mobile",
+      description: "Latest OnePlus Phone",
+      image: "https://m.media-amazon.com/images/I/41g1pQr8xUL._SX300_SY300_QL70_FMwebp_.jpg"
+    },{
+      name: "Nothing Phone2",
+      category: "Mobile",
+      description: "Latest Nothing Phone",
+      image: "https://m.media-amazon.com/images/I/713rQyFyeXL._SX522_.jpg"
+    }
+  ]
+
+  res.render('admin/viewproducts',{admin:true,products});
+});
+
+module.exports = router;
