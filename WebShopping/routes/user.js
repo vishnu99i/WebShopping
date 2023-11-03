8f2ae48e58b698560397891665f02fabfbbfcc75
Vishnu <vinventor1999@gmail.com>
2023-11-03 12:32:17 +0530

    Login and signup page created

--- WebShopping/routes/user.js
+++ WebShopping/routes/user.js
@@ -7,9 +7,18 @@ router.get('/', function(req, res, next) {
 
   productHelpers.getAllProducts().then((products) => {
     console.log(products);
-    res.render('user/viewproducts',{admin:true,products});
-  })
-  
+    res.render('user/viewproducts',{admin: false,products});
+  }) 
 });
+
+//User login
+router.get('/login',(req,res) => {
+  res.render('user/login')
+})
+
+//User signup
+router.get('/signup',(req,res) => {
+  res.render('user/signup')
+})
 
 module.exports = router;
