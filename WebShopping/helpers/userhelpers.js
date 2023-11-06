80d4f1078003306abca50fce113a70c61af67872
Vishnu <vinventor1999@gmail.com>
2023-11-07 01:22:03 +0530

    Cart setup,added items can be seen in database and terminal

--- WebShopping/helpers/userhelpers.js
+++ WebShopping/helpers/userhelpers.js
@@ -1,6 +1,7 @@
 var db = require('../config/connection')
 var collection = require('../config/collections')
 const bcrypt = require('bcrypt')
+var objectId = require('mongodb').ObjectID
 
 module.exports = {
    doSignup: (userData) => {
@@ -35,5 +36,57 @@ module.exports = {
             resolve({status: false})
          }
       })
+   },
+   addToCart: (proId,userId) => {
+      return new Promise(async (resolve,reject) => {
+         let userCart = await db.get().collection(collection.CART_COLLECTION).findOne({user:objectId(userId)})
+         if(userCart){
+            db.get().collection(collection.CART_COLLECTION)
+            .updateOne({user:objectId(userId)},
+               {
+                     $push:{products:objectId(proId)}
+               }
+            ).then((response) => {
+               resolve()
+            })
+         }
+         else{
+            let cartObj = {
+               user:objectId(userId),
+               products:[objectId(proId)]
+            }
+            db.get().collection(collection.CART_COLLECTION).insertOne(cartObj).then((response) => {
+               resolve()
+            })
+         }
+      })
+   },
+   getCartProducts:(userId) => {
+      return new Promise(async(resolve,reject) => {
+         let cartItems = await db.get().collection(collection.CART_COLLECTION).aggregate([
+            {
+               $match:{user:objectId(userId)}
+            },
+            {
+               $lookup:{
+                  from:collection.PRODUCT_COLLECTION,
+                  //Database variable $products
+                  let:{proList:'$products'},
+                  pipeline:[
+                     {
+                        $match:{
+                           $expr:{
+                              $in:['$_id',"$$proList"]
+                           }
+                        }
+                     }
+                  ],
+                  as:'cartItems'
+               }
+            }
+         ]).toArray()
+         resolve(cartItems[0].cartItems)
+      })
    }
+
 }
--- WebShopping/routes/user.js
+++ WebShopping/routes/user.js
@@ -76,8 +76,16 @@ router.get('/logout',(req,res) => {
   res.redirect('/')
 })
 
-router.get('/cart',verifyLogin,(req,res) => {
+router.get('/cart',verifyLogin,async(req,res) => {
+  let products = await userHelpers.getCartProducts(req.session.user._id)
+  console.log(products)
   res.render('user/cart')
+})
+
+router.get('/addtocart/:id',verifyLogin,(req,res) => {
+  userHelpers.addToCart(req.params.id,req.session.user._id).then(() => {
+    res.redirect('/')
+  })
 })
 
 module.exports = router;
